4c0178d46d79ca1c60a68c68e08b4d0d37828ae0
Greg Tener <borisb0ri5@gmail.com>
2014-01-21 08:57:41 -0800

    Get the partition tests working

--- nishe.js
+++ nishe.js
@@ -1,18 +1,41 @@
 'use strict';
 
-var Partition = function() {
-  function Partition() {
-    const image = function() {
+const Partition = function() {
+  function Partition(imagesArg) {
+    const images = imagesArg;
+
+    this.image = function(x) {
+      return images.get(x);
     };
   };
   return {
-    const from: function() {
+    from: function(partition) {
+      const domain = [];
+      for (let i = 0; i < partition.length; i++) {
+        for (let j = 0; j < partition[i].length; j++) {
+          if (domain.indexOf(partition[i][j]) != -1) {
+            return undefined;
+          }
+          domain.push(partition[i][j]);
+        }
+      }
+      domain.sort();
+
+      const images = new Map();
+      let index = 0;
+      for (let i = 0; i < partition.length; i++) {
+        for (let j = 0; j < partition[i].length; j++) {
+          images.set(partition[i][j], domain[index]);
+        }
+        index += partition[i].length;
+      }
+      return new Partition(images);
     }
   };
-};
+}();
 
 if (typeof exports == 'undefined') {
-  var exports = this['nishe'] = {};
+  const exports = this['nishe'] = {};
 }
 
 exports.Partition = Partition;
--- nishe_test.js
+++ nishe_test.js
@@ -9,8 +9,8 @@ describe('nishe', function() {
       it('should match input', function() {
         assert.equal('a', nishe.Partition.from([['a']]).image('a'));
 
-        assert.equal('a', nishe.Partition.from([['a b']]).image('a'));
-        assert.equal('a', nishe.Partition.from([['a b']]).image('b'));
+        assert.equal('a', nishe.Partition.from([['a', 'b']]).image('a'));
+        assert.equal('a', nishe.Partition.from([['a', 'b']]).image('b'));
 
         assert.equal('a', nishe.Partition.from([['a'], ['b']]).image('a'));
         assert.equal('b', nishe.Partition.from([['a'], ['b']]).image('b'));
