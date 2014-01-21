3f43e99ab5b55ee5309e78a971c313d881e7e7ab
Greg Tener <borisb0ri5@gmail.com>
2014-01-21 08:18:39 -0800

    Add initial set of files.

---
+++ nishe.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var Partition = function() {
+  function Partition() {
+    const image = function() {
+    };
+  };
+  return {
+    const from: function() {
+    }
+  };
+};
+
+if (typeof exports == 'undefined') {
+  var exports = this['nishe'] = {};
+}
+
+exports.Partition = Partition;
---
+++ nishe_test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const assert = require('assert');
+const nishe = require('./nishe');
+
+describe('nishe', function() {
+  describe('Partition', function() {
+    describe('#image', function() {
+      it('should match input', function() {
+        assert.equal('a', nishe.Partition.from([['a']]).image('a'));
+
+        assert.equal('a', nishe.Partition.from([['a b']]).image('a'));
+        assert.equal('a', nishe.Partition.from([['a b']]).image('b'));
+
+        assert.equal('a', nishe.Partition.from([['a'], ['b']]).image('a'));
+        assert.equal('b', nishe.Partition.from([['a'], ['b']]).image('b'));
+
+        assert.equal('b', nishe.Partition.from([['b'], ['a']]).image('a'));
+        assert.equal('a', nishe.Partition.from([['b'], ['a']]).image('b'));
+      });
+    });
+  });
+});
