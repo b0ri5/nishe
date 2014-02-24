61cb9b726c9ba6f4a64f78c9f047b32f2dfebe9f
Greg Tener <borisb0ri5@gmail.com>
2014-02-23 23:13:39 -0800

    Add some more tests to ensure reasonable invariants are enforced.

--- nisheviz_test.js
+++ nisheviz_test.js
@@ -7,13 +7,38 @@ define([
   describe('nisheviz', function() {
     describe('SvgPartitionRenderer', function() {
       describe('#render', function() {
-        it('larger partitions should have larger bboxes', function() {
+        it('gives wider partitions wider bboxes', function() {
           var renderer = new nisheviz.SvgPartitionRenderer(SVG('svg'));
-          var pa = nishe.Partition.from([['a']]);
-          var paSvg = renderer.render(pa);
-          var pab = nishe.Partition.from([['a', 'b']]);
-          var pabSvg = renderer.render(pab);
-          paSvg.bbox().width.should.be.lessThan(pabSvg.bbox().width);
+          var a = nishe.Partition.from([['a']]);
+          var aSvg = renderer.render(a);
+          var ab = nishe.Partition.from([['a', 'b']]);
+          var abSvg = renderer.render(ab);
+          aSvg.bbox().width.should.be.lessThan(abSvg.bbox().width);
+        });
+        it('gives shorter partitions shorter bboxes', function() {
+          var renderer = new nisheviz.SvgPartitionRenderer(SVG('svg'));
+          var a = nishe.Partition.from([['a']]);
+          var aSvg = renderer.render(a);
+          var b = nishe.Partition.from([['b']]);
+          var bSvg = renderer.render(b);
+          bSvg.bbox().height.should.not.be.lessThan(aSvg.bbox().height);
+        });
+        it('gives finer partitions wider bboxes', function() {
+          var renderer = new nisheviz.SvgPartitionRenderer(SVG('svg'));
+          var ab = nishe.Partition.from([['a', 'b']]);
+          var abSvg = renderer.render(ab);
+          var abFiner = nishe.Partition.from([['a'], ['b']]);
+          var abFinerSvg = renderer.render(abFiner);
+          abSvg.bbox().width.should.not.be.lessThan(abFinerSvg.bbox().width);
+        });
+        it('gives identical partitions identical bboxes', function() {
+          var renderer = new nisheviz.SvgPartitionRenderer(SVG('svg'));
+          var a = nishe.Partition.from([['a']]);
+          var aSvg = renderer.render(a);
+          var a2 = nishe.Partition.from([['a']]);
+          var a2Svg = renderer.render(a2);
+          aSvg.bbox().width.should.equal(a2Svg.bbox().width);
+          aSvg.bbox().height.should.equal(a2Svg.bbox().height);
         });
       });
     });
