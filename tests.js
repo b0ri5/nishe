9f32bd52b4b275392daea2116d0f6ef965a0b3e2
Greg Tener <borisb0ri5@gmail.com>
2014-03-24 06:18:23 +0000

    Remove nisheviz and use jshint

--- tests.js
+++ tests.js
@@ -1,6 +1,7 @@
-require(['third_party/mocha'], function() {
+require(['node_modules/mocha/mocha'], function() {
+  'use strict';
   mocha.setup('bdd');
-  require(['nishe_test', 'nisheviz_test'], function() {
+  require(['nishe_test'], function() {
     if (window.mochaPhantomJS) {
       mochaPhantomJS.run();
     } else {
