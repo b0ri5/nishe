faab1dc747ac139e7c6ac4160395a073276f9958
Greg Tener <borisb0ri5@gmail.com>
2015-07-17 14:04:20 +0000

    Use let instead of var in the test

--- src/nishe_test.ts
+++ src/nishe_test.ts
@@ -1,5 +1,5 @@
 ///<reference path='../node_modules/immutable/dist/immutable.d.ts'/>
-///<reference path="../typings/jasmine/jasmine.d.ts" />
+///<reference path='../typings/jasmine/jasmine.d.ts'/>
 
 import nishe = require('nishe');
 import Immutable = require('immutable');
@@ -7,7 +7,7 @@ import Immutable = require('immutable');
 describe('Partition', () => {
   describe('domain', () => {
     it('matches constructor', () => {
-      var p = new nishe.Partition(Immutable.Map({'a': 'a'}));
+      let p = new nishe.Partition(Immutable.Map({'a': 'a'}));
       expect(p.domain().toArray()).toEqual(['a']);
     });
     it('throws if not a partition', () => {
